b13a013ef8aa4c8fe1a920af4d0ec7d4b926c667
Kiseev Nikolay <kiseev.nikolay@gmail.com>
2021-04-16 21:26:00 +0300

    Fix groups

--- accounts/views.py
+++ accounts/views.py
@@ -10,12 +10,18 @@ import main.views
 
 def sign_up(request):
     context = {}
+    group_name = 'common users'
 
     form = UserCreationForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
             user = form.save()
-            group = Group.objects.get(name='common users')
+            groups = Group.objects.filter(name=group_name)
+            if len(groups) > 0:
+                group = groups[0]
+            else:
+                group = Group(name=group_name)
+                group.save()
             group.user_set.add(user)
 
             return HttpResponseRedirect(reverse('login'))
